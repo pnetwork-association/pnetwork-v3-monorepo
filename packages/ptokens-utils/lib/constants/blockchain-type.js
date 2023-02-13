b60e192f171e9d8e4503d3a79ef9d457f50eca7c
Alain Olivier <oliviera9@gmail.com>
2023-02-13 11:22:06 +0100

    feat(ptokens-utils): add blockchain type constants

---
+++ packages/ptokens-utils/lib/constants/blockchain-type.js
@@ -0,0 +1,6 @@
+module.exports = {
+  EVM: 'EVM',
+  EOSIO: 'EOSIO',
+  ALGORAND: 'ALGORAND',
+  UTXO: 'UTXO',
+}
--- packages/ptokens-utils/lib/constants/index.js
+++ packages/ptokens-utils/lib/constants/index.js
@@ -2,10 +2,12 @@ const misc = require('./misc')
 const sides = require('./bridge-sides')
 const loggerFormats = require('./logger-formats')
 const metadataChainIds = require('./metadata-chain-ids')
+const blockchainType = require('./blockchain-type')
 
 module.exports = {
   ...misc,
   ...sides,
   loggerFormats,
   metadataChainIds,
+  blockchainType,
 }
