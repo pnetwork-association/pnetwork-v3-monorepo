54bcfcd8060ba41020219152661b61c20b400845
Alain Olivier <oliviera9@gmail.com>
2023-02-23 11:29:03 +0100

    refactor(ptokens-listener): validate configuration against one schema

--- packages/ptokens-listener/lib/check-configuration.js
+++ packages/ptokens-listener/lib/check-configuration.js
@@ -3,12 +3,8 @@ const configurationSchema = require('./schemas/listener-configuration')
 const { validation } = require('ptokens-utils')
 const { logger } = require('./get-logger')
 
-const validSchemas = [configurationSchema]
-
 const validateConfiguration = _config =>
-  Promise.any(
-    validSchemas.map(_schema => validation.validateJson(_schema, _config))
-  ).then(() => _config)
+  validation.validateJson(configurationSchema, _config).then(() => _config)
 
 module.exports.checkConfiguration = () =>
   Promise.resolve(getConfiguration())
