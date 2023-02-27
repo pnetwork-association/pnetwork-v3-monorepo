657e953a5f0e18e75aedd57f6d08b77252d2f74d
Alain Olivier <oliviera9@gmail.com>
2023-02-27 16:43:38 +0100

    refactor(ptokens-listener): remove unnecessary semicolon

--- packages/ptokens-listener/lib/setup-exit-listeners.js
+++ packages/ptokens-listener/lib/setup-exit-listeners.js
@@ -5,18 +5,15 @@ const exitCleanly = _exitCode =>
   // eslint-disable-next-line no-process-exit
   shutDownLogging().then(_ => process.exit(_exitCode))
 
-const setupExitEventListeners = () => {
-  ;['SIGINT', 'SIGTERM'].map(_signal => {
-    process.on(_signal, () => {
-      logger.info(`${_signal} caught! Exiting...`)
-      return exitCleanly(0)
+const setupExitEventListeners = () =>
+  Promise.all(
+    ['SIGINT', 'SIGTERM'].map(_signal => {
+      process.on(_signal, () => {
+        logger.info(`${_signal} caught! Exiting...`)
+        return exitCleanly(0)
+      })
     })
-  })
-
-  logger.debug('Exit listeners set!')
-
-  return Promise.resolve()
-}
+  ).then(_ => logger.debug('Exit listeners set!'))
 
 module.exports = {
   exitCleanly,
