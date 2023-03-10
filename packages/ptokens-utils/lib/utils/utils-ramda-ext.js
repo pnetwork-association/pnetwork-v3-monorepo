b70bcad97355a95f449974fac7c4495e4f30ee06
gitmp01 <mauro.piazza.01@gmail.com>
2023-03-10 18:12:13 +0000

    feat(ptokens-utils): add `hasNot` (utils-ramda-ext.js)

--- packages/ptokens-utils/lib/utils/utils-ramda-ext.js
+++ packages/ptokens-utils/lib/utils/utils-ramda-ext.js
@@ -1,10 +1,13 @@
-const { not, isNil, isEmpty } = require('ramda')
+const { has, not, isNil, isEmpty } = require('ramda')
+
+const hasNot = _something => not(has(_something))
 
 const isNotNil = _something => not(isNil(_something))
 
 const isNotEmpty = _something => not(isEmpty(_something))
 
 module.exports = {
+  hasNot,
   isNotNil,
   isNotEmpty,
 }
