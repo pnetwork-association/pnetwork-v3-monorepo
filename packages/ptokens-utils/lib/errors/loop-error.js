0aa6692a52be4494bd6b4656873aec588ae49845
gitmp01 <mauro.piazza.01@gmail.com>
2023-03-13 14:38:34 +0000

    chore(ptokens-request-processor): propagate the stacktrace (loop-error.js)

--- packages/ptokens-utils/lib/errors/loop-error.js
+++ packages/ptokens-utils/lib/errors/loop-error.js
@@ -1,6 +1,19 @@
+/**
+ * Useful to throw errors when inside a loop and
+ * get the state object included in the error.
+ *
+ * Usage:
+ *
+ *   loop(err.message, stateObject)
+ *
+ * or in a catch, in order to deliver the stacktrace
+ *
+ *   .catch(_err => new LoopError('Loop failed!', {}, _err))
+ */
 class LoopError extends Error {
-  constructor(_msg, _lastLoopState) {
-    super(_msg)
+  constructor(_msg, _lastLoopState, _err = null) {
+    // cause delivers the stacktrace
+    super(_msg, { cause: _err })
     const { isNotNil } = require('../utils')
     this.lastLoopState =
       isNotNil(_lastLoopState) && _lastLoopState.length > 0
