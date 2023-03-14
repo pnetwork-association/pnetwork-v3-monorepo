dd899d80516d1cfee0efa78c48825d8bda57db79
gitmp01 <mauro.piazza.01@gmail.com>
2023-03-14 09:55:17 +0000

    feat(ptokens-utils): add `doesNotInclude`

--- packages/ptokens-utils/lib/utils/utils-ramda-ext.js
+++ packages/ptokens-utils/lib/utils/utils-ramda-ext.js
@@ -1,4 +1,4 @@
-const { has, not, isNil, isEmpty } = require('ramda')
+const { has, not, isNil, isEmpty, curry, includes } = require('ramda')
 
 const hasNot = _something => not(has(_something))
 
@@ -6,8 +6,13 @@ const isNotNil = _something => not(isNil(_something))
 
 const isNotEmpty = _something => not(isEmpty(_something))
 
+const doesNotInclude = curry((_something, _list) =>
+  not(includes(_something, _list))
+)
+
 module.exports = {
   hasNot,
   isNotNil,
   isNotEmpty,
+  doesNotInclude,
 }
