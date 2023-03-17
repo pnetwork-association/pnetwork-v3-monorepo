c91ec4a125c0a45595131726c97845a3e3367f6f
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-03-17 19:27:58 +0000

    refactor(ptokens-request-processor): use schema constants

--- packages/ptokens-request-processor/test/evm/evm-build-proposals-txs.test.js
+++ packages/ptokens-request-processor/test/evm/evm-build-proposals-txs.test.js
@@ -28,22 +28,25 @@ describe('Build proposals test for EVM', () => {
       const issuanceManagerAddress = ethers.Wallet.createRandom().address
       const redeemManagerAddress = ethers.Wallet.createRandom().address
       const eventReport = {
-        status: 'detected',
-        amount: '1111111',
-        eventName: 'redeem',
-        originatingChainId: '0x005fe7f9',
-        destinationChainId: '0x01ec97de',
-        destinationAddress: '11eXzETyUxiQPXwU2udtVFQFrFjgRhhvPj',
-        originatingTransactionHash:
-          '0x9488dee8cb5c6b2f6299e45e48bba580f46dbd496cfaa70a182060fd5dc81cb4',
-        tokenAddress: '0xdaacb0ab6fb34d24e8a67bfa14bf4d95d4c7af92',
-        originatingAddress: '0x9f5377fa03dcd4016a33669b385be4d0e02f27bc',
-        witnessedTimestamp: '2023-03-07T16:11:38.835Z',
-        userData: null,
-        finalTransactionHash: null,
-        proposedTransactionHash: null,
-        proposedTransactionTimestamp: null,
-        finalTransactionTimestamp: null,
+        [schemas.constants.SCHEMA_STATUS_KEY]: 'detected',
+        [schemas.constants.SCHEMA_AMOUNT_KEY]: '1111111',
+        [schemas.constants.SCHEMA_EVENT_NAME_KEY]: 'redeem',
+        [schemas.constants.SCHEMA_ORIGINATING_CHAIN_ID_KEY]: '0x005fe7f9',
+        [schemas.constants.SCHEMA_DESTINATION_CHAIN_ID_KEY]: '0x01ec97de',
+        [schemas.constants.SCHEMA_DESTINATION_ADDRESS_KEY]:
+          '11eXzETyUxiQPXwU2udtVFQFrFjgRhhvPj',
+        [schemas.constants.SCHEMA_ORIGINATING_TX_HASH_KEY]:
+          '0x9488dee8cb5c6b2f6299e45e48bba580f46dbd496cfaa70a182060fd5dc81cb4]',
+        [schemas.constants.SCHEMA_TOKEN_ADDRESS_KEY]:
+          '0xdaacb0ab6fb34d24e8a67bfa14bf4d95d4c7af92',
+        [schemas.constants.SCHEMA_ORIGINATING_ADDRESS_KEY]:
+          '0x9f5377fa03dcd4016a33669b385be4d0e02f27bc',
+        [schemas.constants.SCHEMA_WITNESSED_TS_KEY]: '2023-03-07T16:11:38.835Z',
+        [schemas.constants.SCHEMA_USER_DATA_KEY]: null,
+        [schemas.constants.SCHEMA_FINAL_TX_HASH_KEY]: null,
+        [schemas.constants.SCHEMA_PROPOSAL_TS_KEY]: null,
+        [schemas.constants.SCHEMA_PROPOSAL_TX_HASH_KEY]: null,
+        [schemas.constants.SCHEMA_FINAL_TX_TS_KEY]: null,
       }
 
       await validation.validateJson(schemas.db.collections.events, eventReport)
