ad27e60e23aeb64654cc6e0a5323502ecbc2f49c
Alain Olivier <oliviera9@gmail.com>
2023-03-20 18:28:01 +0100

    refactor(ptokens-request-processor): rename filterIsInvalidRequest() to isRequestInvalid()
    Co-authored-by: Mauro Piazza <mauro.piazza.01@gmail.com>

--- packages/ptokens-request-processor/lib/evm/evm-validate-queued-requests.js
+++ packages/ptokens-request-processor/lib/evm/evm-validate-queued-requests.js
@@ -13,7 +13,7 @@ const checkRequestAgainstMatchingReport = (_report, _request) =>
   _report[schemas.constants.SCHEMA_DESTINATION_ADDRESS_KEY] ===
     _request[schemas.constants.SCHEMA_DESTINATION_ADDRESS_KEY]
 
-const filterIsInvalidRequest = curry((_detectedTxs, _request) => {
+const isRequestInvalid = curry((_detectedTxs, _request) => {
   const matchingReport = _detectedTxs.find(
     _element =>
       _element[schemas.constants.SCHEMA_ORIGINATING_TX_HASH_KEY] ===
@@ -28,9 +28,7 @@ const filterOutInvalidQueuedRequestsAndPutInState = _state => {
   logger.info('Getting EVM on chain requests and putting in state...')
   const onChainRequests = _state[STATE_ONCHAIN_REQUESTS_KEY]
   const detectedTxs = _state[STATE_DETECTED_DB_REPORTS_KEY]
-  const invalidRequests = onChainRequests.filter(
-    filterIsInvalidRequest(detectedTxs)
-  )
+  const invalidRequests = onChainRequests.filter(isRequestInvalid(detectedTxs))
   // TODO: filter out on chain requests
   return Promise.resolve(
     assoc(STATE_TO_BE_DISMISSED_REQUESTS_KEY, invalidRequests, _state)
