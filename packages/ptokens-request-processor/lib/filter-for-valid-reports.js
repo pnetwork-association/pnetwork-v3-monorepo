38fcaa36b23b67433bb0e6b551340628094ae18b
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-03-22 19:17:14 +0000

    chore(ptokens-request-processor): rm obsolete .then

--- packages/ptokens-request-processor/lib/filter-for-valid-reports.js
+++ packages/ptokens-request-processor/lib/filter-for-valid-reports.js
@@ -5,7 +5,6 @@ const { utils, validation } = require('ptokens-utils')
 const validateReportOrSetNull = _report =>
   validation
     .validateJson(schemas.db.collections.events, _report)
-    .then(_ => _report)
     .catch(_err => {
       logger.warn('Invalid report detected, skipping...', _err)
       logger.warn(JSON.stringify(_report))
