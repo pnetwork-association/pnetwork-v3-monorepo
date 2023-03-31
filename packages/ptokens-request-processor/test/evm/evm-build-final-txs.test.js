25a02c8f01558de7ce27d683e0a3a6e7d0bf88d8
Alain Olivier <oliviera9@gmail.com>
2023-03-31 19:10:01 +0200

    test(ptokens-request-processor): fix failing test for makeFinalContractCall()

--- packages/ptokens-request-processor/test/evm/evm-build-final-txs.test.js
+++ packages/ptokens-request-processor/test/evm/evm-build-final-txs.test.js
@@ -10,35 +10,38 @@ describe('General final txs testing', () => {
       const ethers = jestMockEthers()
       const finalizedTxHash =
         '0xd656ffac17b71e2ea2e24f72cd4c15c909a0ebe1696f8ead388eb268268f1cbf'
-      const expectedObject = { transactionHash: finalizedTxHash }
+      const expectedObject = { hash: finalizedTxHash }
 
-      const mockCallIssue = jest.fn().mockResolvedValue({
+      const mockExecute = jest.fn().mockResolvedValue({
         wait: jest.fn().mockResolvedValue(expectedObject),
       })
 
-      ethers.Contract = jestMockContractConstructor('callIssue', mockCallIssue)
+      ethers.Contract = jestMockContractConstructor(
+        'protocolExecuteOperation',
+        mockExecute
+      )
 
       const {
         makeFinalContractCall,
       } = require('../../lib/evm/evm-build-final-txs')
 
       const wallet = ethers.Wallet.createRandom()
-      const issuanceManagerAddress =
-        '0xbae4957b7f913bdae17b31d8f32991ff88a12e37'
-      const redeemManagerAddress = '0x341aa660fd5c280f5a9501e3822bb4a98e816d1b'
+      const stateManagerAddress = '0xbae4957b7f913bdae17b31d8f32991ff88a12e37'
       const eventReport = {
         [schemas.constants.SCHEMA_ID_KEY]:
           '0x005fe7f9_0x9488dee8cb5c6b2f6299e45e48bba580f46dbd496cfaa70a182060fd5dc81cb4',
         [schemas.constants.SCHEMA_STATUS_KEY]: 'proposed',
-        [schemas.constants.SCHEMA_AMOUNT_KEY]: '1111111',
-        [schemas.constants.SCHEMA_EVENT_NAME_KEY]: 'pegin',
-        [schemas.constants.SCHEMA_UNDERLYING_ASSET_CHAIN_ID_KEY]: '0x00000001',
+        [schemas.constants.SCHEMA_ASSET_AMOUNT_KEY]: '1111111',
+        [schemas.constants.SCHEMA_EVENT_NAME_KEY]:
+          schemas.db.enums.eventNames.USER_OPERATION,
+        [schemas.constants.SCHEMA_UNDERLYING_ASSET_NETWORK_ID_KEY]:
+          '0x00000001',
         [schemas.constants.SCHEMA_DESTINATION_NETWORK_ID_KEY]: '0x01ec97de',
-        [schemas.constants.SCHEMA_DESTINATION_ADDRESS_KEY]:
+        [schemas.constants.SCHEMA_DESTINATION_ACCOUNT_KEY]:
           '11eXzETyUxiQPXwU2udtVFQFrFjgRhhvPj',
         [schemas.constants.SCHEMA_ORIGINATING_TX_HASH_KEY]:
           '0x9488dee8cb5c6b2f6299e45e48bba580f46dbd496cfaa70a182060fd5dc81cb4]',
-        [schemas.constants.SCHEMA_TOKEN_ADDRESS_KEY]:
+        [schemas.constants.SCHEMA_ASSET_TOKEN_ADDRESS_KEY]:
           '0xdaacb0ab6fb34d24e8a67bfa14bf4d95d4c7af92',
         [schemas.constants.SCHEMA_ORIGINATING_ADDRESS_KEY]:
           '0x9f5377fa03dcd4016a33669b385be4d0e02f27bc',
@@ -54,8 +57,7 @@ describe('General final txs testing', () => {
       const timeout = 5000
       const result = await makeFinalContractCall(
         wallet,
-        issuanceManagerAddress,
-        redeemManagerAddress,
+        stateManagerAddress,
         timeout,
         eventReport
       )
