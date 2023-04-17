c017e9b3bc84f65443c168c47f50d2cfcaaa6ce6
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-04-17 11:05:39 +0100

    chore(ptokens-utils): add usage

--- packages/ptokens-utils/lib/utils/bench-tick.js
+++ packages/ptokens-utils/lib/utils/bench-tick.js
@@ -1,6 +1,20 @@
 /* eslint-disable no-console */
 const deltas = []
 
+/**
+ * Usage:
+ * utils.bench.tickSync()
+ * codeToBench1()
+ * utils.bench.tickSync()
+ * codeToBench2()
+ * utils.bench.diffSync()
+ *
+ * Output:
+ * Starting time: 2023-10-21T21:01:12
+ *   tick 1: 1ms
+ *   tick 2: 4ms
+ *
+ **/
 const tickSync = () => {
   if (!deltas) {
     console.log('Ticking started')
