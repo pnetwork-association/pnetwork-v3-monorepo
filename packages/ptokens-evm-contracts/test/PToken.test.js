a5c6424b9b2d6c11cfc7ba3a835b061ce390d820
Alessandro Manfredi <alle.manfredi@gmail.com>
2023-04-20 09:05:15 +0200

    feat(ptokens-evm-contracts): adds some PToken tests

--- packages/ptokens-evm-contracts/test/PToken.test.js
+++ packages/ptokens-evm-contracts/test/PToken.test.js
@@ -1,4 +1,4 @@
-/*const { expect } = require('chai')
+const { expect } = require('chai')
 const { ethers } = require('hardhat')
 const { time } = require('@nomicfoundation/hardhat-network-helpers')
 
@@ -95,4 +95,3 @@ describe('PToken', () => {
     })
   }
 })
-*/
