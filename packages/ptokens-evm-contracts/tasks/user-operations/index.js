6b7d70057100aef5edabad2d127822972a35948e
eenvin <envin@proton.me>
2023-05-16 11:40:37 +0200

    feat(ptokens-evm-contracts): add state manager operations task

--- packages/ptokens-evm-contracts/tasks/user-operations/index.js
+++ packages/ptokens-evm-contracts/tasks/user-operations/index.js
@@ -1,5 +1,7 @@
 const mintAndBurn = require('./router-user-send-mint-and-burn.task.js')
+const protocolExecuteOperation = require('./statemanager-protocol-queue-exev.task.js')
 
 module.exports = {
   ...mintAndBurn,
+  ...protocolExecuteOperation
 }
