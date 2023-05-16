a553c1c705745fa0f6a3f323a3aa169be7ed6b3b
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-05-16 17:09:18 +0100

    chore(ptoken-evm-contracts): deploy just the PToken, nothing else

--- packages/ptokens-evm-contracts/tasks/deploy/deploy-ptoken.task.js
+++ packages/ptokens-evm-contracts/tasks/deploy/deploy-ptoken.task.js
@@ -1,5 +1,6 @@
 const {
   KEY_ADDRESS,
+  KEY_PFACTORY,
   KEY_NETWORK_ID,
   CONTRACT_NAME_PTOKEN,
   TASK_DESC_DEPLOY_PTOKEN,
@@ -8,55 +9,105 @@ const {
   KEY_PROUTER,
   KEY_STATEMANAGER,
   KEY_PTOKEN_LIST,
-  TASK_NAME_DEPLOY_PROUTER,
-  TASK_NAME_DEPLOY_STATEMANAGER,
-  TASK_NAME_CONFIG_PFACTORY,
+  KEY_UNDERLYING_ASSET_LIST,
 } = require('../constants')
+const R = require('ramda')
 const { types } = require('hardhat/config')
-const { deployPFactoryTask } = require('./deploy-pfactory.task')
 const { getConfiguration } = require('./lib/configuration-manager')
 
-const getUnderlyingAssetContract = async (underlyingAssetAddress, underlyingAssetChainName, hre) => {
-  const originalChainName = hre.network.name
-  hre.changeNetwork(underlyingAssetChainName)
-  const ERC20 = await hre.ethers.getContractFactory('ERC20')
-  const token = await ERC20.attach(underlyingAssetAddress)
-  const name = await token.name()
-  const symbol = await token.symbol()
-  const decimals = await token.decimals()
-  hre.changeNetwork(originalChainName)
-  return {name: name.toString(), symbol: symbol.toString(), decimals: decimals.toString()}
-};
+// TODO: export to ptokens-utils
+const rejectIfNil = R.curry((_errMsg, _thing) =>
+  R.isNil(_thing) ? Promise.reject(new Error(_errMsg)) : Promise.resolve(_thing)
+)
 
-const deployPTokenTask = ({ tokenAddress, underlyingAssetChainName, challengePeriod = '120' }, hre) =>
-  deployPFactoryTask(null, hre)
-    .then(_ => hre.run(TASK_NAME_DEPLOY_PROUTER))
-    .then(_ => hre.run(TASK_NAME_DEPLOY_STATEMANAGER, {challengePeriod: challengePeriod}))
-    .then(_ => hre.run(TASK_NAME_CONFIG_PFACTORY))
-    .then(_ => Promise.all([getUnderlyingAssetContract(tokenAddress, underlyingAssetChainName, hre), getConfiguration()]))
-    .then(([_props, _config]) =>
-      console.info('Deploying pToken ...') ||
-      hre.run(TASK_NAME_DEPLOY_ASSET, {
-        configurableName: KEY_PTOKEN_LIST,
-        contractFactoryName: CONTRACT_NAME_PTOKEN,
-        deployArgsArray: [
-          _props.name,
-          _props.symbol,
-          _props.decimals,
-          tokenAddress,
-          _config.get(underlyingAssetChainName)[KEY_NETWORK_ID],
-          // _config.get(hre.network.name)[KEY_PROUTER][KEY_ADDRESS],
-          // _config.get(hre.network.name)[KEY_STATEMANAGER][KEY_ADDRESS],
-        ],
-      })
+// TODO: export to ptokens-utils
+const rejectIfLength0 = R.curry((_errMsg, _array) =>
+  _array.length === 0 ? Promise.reject(new Error(_errMsg)) : Promise.resolve(_array)
+)
+
+const getPFactoryAddress = (hre, _config) =>
+  Promise.resolve(_config.get(hre.network.name))
+    .then(R.path([KEY_PFACTORY, KEY_ADDRESS]))
+    .then(rejectIfNil(`Could not find any ${KEY_PFACTORY} address for '${hre.network.name}' network, is it deployed?`))
+    .then(_address => console.info(`Found ${KEY_PFACTORY} @ ${_address}`) || _address)
+
+const getPRouterAddress = (hre, _config) =>
+  Promise.resolve(_config.get(hre.network.name))
+    .then(R.path([KEY_PROUTER, KEY_ADDRESS]))
+    .then(rejectIfNil(`Could not find any ${KEY_PROUTER} address for '${hre.network.name}' network, is it deployed?`))
+    .then(_address => console.info(`Found ${KEY_PROUTER} @ ${_address}`) || _address)
+
+const getStateManagerAddress = (hre, _config) =>
+  Promise.resolve(_config.get(hre.network.name))
+    .then(R.path([KEY_STATEMANAGER, KEY_ADDRESS]))
+    .then(rejectIfNil(`Could not find any ${KEY_STATEMANAGER} address for '${hre.network.name}' network, is it deployed?`))
+    .then(_address => console.info(`Found ${KEY_STATEMANAGER} @ ${_address}`) || _address)
+
+const isAssetAddressEqualTo = _address => R.compose(R.equals(_address), R.prop(KEY_ADDRESS))
+
+const getUnderlyingAsset = (_chainName, _config, _underlyingAssetAddress) =>
+  Promise.resolve(_config.get(_chainName))
+    .then(R.prop(KEY_UNDERLYING_ASSET_LIST))
+    .then(R.filter(isAssetAddressEqualTo(_underlyingAssetAddress)))
+    .then(
+      rejectIfLength0(`Could not find any underlying asset with address ${_underlyingAssetAddress}`)
+    )
+    .then(R.prop(0))
+    .then(
+      _underlyingAsset =>
+        console.info(`Underlying asset for ${_underlyingAssetAddress} found!`) || _underlyingAsset
+    )
+
+const deployPTokenTask = ({ underlyingAssetAddress, underlyingChain }, hre) =>
+  getConfiguration()
+    .then(_config =>
+      Promise.all([
+        getPFactoryAddress(hre, _config),
+        getPRouterAddress(hre, _config),
+        getStateManagerAddress(hre, _config),
+        _config.get(underlyingChain)[KEY_NETWORK_ID],
+        getUnderlyingAsset(underlyingChain, _config, underlyingAssetAddress),
+      ])
+    )
+    .then(
+      ([
+        _pFactoryAddress,
+        _pRouterAddress,
+        _pStateManagerAddress,
+        _underlyingAssetNetworkId,
+        _underlyingAsset,
+      ]) =>
+        hre.run(TASK_NAME_DEPLOY_ASSET, {
+          configurableName: KEY_PTOKEN_LIST,
+          contractFactoryName: CONTRACT_NAME_PTOKEN,
+          deployArgsArray: [
+            _underlyingAsset.name,
+            _underlyingAsset.symbol,
+            _underlyingAsset.decimals,
+            _underlyingAsset.address,
+            _underlyingAssetNetworkId,
+            // _pRouterAddress,
+            // _pStateManagerAddress,
+          ],
+        })
     )
 
 task(TASK_NAME_DEPLOY_PTOKEN, TASK_DESC_DEPLOY_PTOKEN, deployPTokenTask)
   .addPositionalParam(
-    'tokenAddress',
-    'Underlying token asset we want to wrap',
+    'underlyingAssetAddress',
+    'Underlying asset asset address we want to wrap',
     undefined,
     types.string
   )
-  .addPositionalParam('underlyingAssetChainName', 'Underlying Asset chain name', undefined, types.string)
-  .addOptionalParam("challengePeriod", "Define a challenge period for the state manager if not already deployed", undefined, types.string)
+  .addPositionalParam(
+    'underlyingChain',
+    'Underlying Asset chain name',
+    undefined,
+    types.string
+  )
+  .addOptionalParam(
+    'challengePeriod',
+    'Define a challenge period for the state manager if not already deployed',
+    undefined,
+    types.string
+  )
