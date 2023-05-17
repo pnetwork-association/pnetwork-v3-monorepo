1471c29a8415b21315fe952093e6237341d8e959
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-05-17 15:32:48 +0100

    chore(ptokens-evm-contracts): add PRouter address to PFactory

--- packages/ptokens-evm-contracts/tasks/deploy/deploy-prouter.task.js
+++ packages/ptokens-evm-contracts/tasks/deploy/deploy-prouter.task.js
@@ -5,17 +5,32 @@ const {
   TASK_NAME_DEPLOY_PROUTER,
   TASK_NAME_DEPLOY_CONTRACT,
 } = require('../constants')
+const R = require('ramda')
 const { deployPFactoryTask } = require('./deploy-pfactory.task')
 
-const deployPRouterTask = (_, hre) =>
-  console.info('Deploying pRouter ...') ||
-  deployPFactoryTask(null, hre).then(_pFactory =>
-    hre.run(TASK_NAME_DEPLOY_CONTRACT, {
-      configurableName: KEY_PROUTER,
-      contractFactoryName: CONTRACT_NAME_PROUTER,
-      deployArgsArray: [_pFactory.address],
-    })
-  )
+const setRouterAddressInPFactory = R.curry(
+  (_pFactory, _pRouter) =>
+    console.info('Setting pRouter address in pFactory...') ||
+    _pFactory
+      .setRouter(_pRouter.address)
+      .then(_tx => _tx.wait())
+      .then(_tx => console.info(`Tx mined @ ${_tx.transactionHash}`))
+      .then(_ => _pRouter)
+)
+
+const deployPRouter = R.curry(
+  (hre, _pFactory) =>
+    console.info('Deploying pRouter ...') ||
+    hre
+      .run(TASK_NAME_DEPLOY_CONTRACT, {
+        configurableName: KEY_PROUTER,
+        contractFactoryName: CONTRACT_NAME_PROUTER,
+        deployArgsArray: [_pFactory.address],
+      })
+      .then(setRouterAddressInPFactory(_pFactory))
+)
+
+const deployPRouterTask = (_, hre) => deployPFactoryTask(null, hre).then(deployPRouter(hre))
 
 task(TASK_NAME_DEPLOY_PROUTER, TASK_DESC_DEPLOY_PROUTER, deployPRouterTask)
 
