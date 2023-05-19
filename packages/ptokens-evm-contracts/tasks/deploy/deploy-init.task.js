24a0131635d43d13d4f5c621261b10850ac364df
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-05-19 11:55:45 +0100

    chore(ptokens-evm-contracts): add empty list when initializing

--- packages/ptokens-evm-contracts/tasks/deploy/deploy-init.task.js
+++ packages/ptokens-evm-contracts/tasks/deploy/deploy-init.task.js
@@ -1,4 +1,5 @@
 const R = require('ramda')
+const { KEY_PTOKEN_LIST, KEY_UNDERLYING_ASSET_LIST } = require('../constants')
 const { getConfiguration, updateConfiguration } = require('./lib/configuration-manager')
 
 const getSelectedChainId = hre => hre.network.config.chainId
@@ -22,9 +23,19 @@ const maybeAddNewNetwork = R.curry((hre, _config) =>
   !_config.has(hre.network.name) ? addNewNetwork(hre, _config) : Promise.resolve(_config)
 )
 
+const maybeAddEmptyUnderlyingAssetList = R.curry((hre, _config) =>
+  _config.union(`${hre.network.name}.${KEY_UNDERLYING_ASSET_LIST}`, [])
+)
+
+const maybeAddEmptyPTokenList = R.curry((hre, _config) =>
+  _config.union(`${hre.network.name}.${KEY_PTOKEN_LIST}`, [])
+)
+
 const deployInit = (_, hre) =>
   getConfiguration()
     .then(maybeAddNewNetwork(hre))
+    .then(maybeAddEmptyPTokenList(hre))
+    .then(maybeAddEmptyUnderlyingAssetList(hre))
     .then(R.prop('data'))
     .then(R.prop(hre.network.name))
 
