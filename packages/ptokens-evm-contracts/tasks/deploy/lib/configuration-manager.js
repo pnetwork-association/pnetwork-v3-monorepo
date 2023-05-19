2ac54aff6a04a917d1b36771f42d44cfc9c443c1
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-05-19 11:57:44 +0100

    refactor(ptokens-evm-contracts): factor out props requiring unions

--- packages/ptokens-evm-contracts/tasks/deploy/lib/configuration-manager.js
+++ packages/ptokens-evm-contracts/tasks/deploy/lib/configuration-manager.js
@@ -5,6 +5,8 @@ const { KEY_PTOKEN_LIST, KEY_UNDERLYING_ASSET_LIST } = require('../../constants'
 
 const getConfiguration = () => Promise.resolve(Store({ path: process.cwd() + PATH_CONFIG_FILE }))
 
+const PROPERTIES_REQUIRING_UNION = [KEY_PTOKEN_LIST, KEY_UNDERLYING_ASSET_LIST]
+
 const updateConfiguration = (...vargs) =>
   new Promise(resolve => {
     const config = vargs.at(0)
@@ -12,16 +14,11 @@ const updateConfiguration = (...vargs) =>
     const typeArgIndex = -2
     const value = vargs.at(valueArgIndex)
     const args = R.slice(1, valueArgIndex, vargs)
+    const propertyChangingValue = vargs.at(typeArgIndex)
     const path = args.reduce((acc, cur) => acc + '.' + cur)
 
-    if (
-      vargs.at(typeArgIndex) === KEY_PTOKEN_LIST ||
-      vargs.at(typeArgIndex) === KEY_UNDERLYING_ASSET_LIST
-    ) {
-      config.union(path, value)
-    } else {
-      config.set(path, value)
-    }
+    if (R.includes(propertyChangingValue, PROPERTIES_REQUIRING_UNION)) config.union(path, value)
+    else config.set(path, value)
 
     return resolve(config)
   })
