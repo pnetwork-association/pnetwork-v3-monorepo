4b971fbbfbec73c84d0e843661727e87d94abe5d
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-05-19 11:56:43 +0100

    refactor(ptokens-evm-contracts): detach from subtask & make it standalone

--- packages/ptokens-evm-contracts/tasks/deploy/deploy-ptoken.task.js
+++ packages/ptokens-evm-contracts/tasks/deploy/deploy-ptoken.task.js
@@ -10,10 +10,22 @@ const {
   KEY_STATEMANAGER,
   KEY_PTOKEN_LIST,
   KEY_UNDERLYING_ASSET_LIST,
+  CONTRACT_NAME_PFACTORY,
+  KEY_ASSET_NAME,
+  KEY_ASSET_SYMBOL,
+  KEY_ASSET_DECIMALS,
 } = require('../constants')
 const R = require('ramda')
 const { types } = require('hardhat/config')
+const assetTypes = require('./lib/asset-types')
+const { deployPToken } = require('../../test/utils')
+const { attachToUnderlyingAsset } = require('./deploy-asset.task')
 const { getConfiguration } = require('./lib/configuration-manager')
+
+const TASK_PARAM_GAS = 'gas'
+const TASK_PARAM_GASPRICE = 'gasPrice'
+const TASK_PARAM_UNDERLYING_ASSET_ADDRESS = 'underlyingAssetAddress'
+const TASK_PARAM_UNDERLYING_ASSET_CHAIN_NAME = 'underlyingAssetChainName'
 
 // TODO: export to ptokens-utils
 const rejectIfNil = R.curry((_errMsg, _thing) =>
@@ -35,83 +47,122 @@ const getPFactoryAddress = (hre, _config) =>
     )
     .then(_address => console.info(`Found ${KEY_PFACTORY} @ ${_address}`) || _address)
 
-const getPRouterAddress = (hre, _config) =>
-  Promise.resolve(_config.get(hre.network.name))
-    .then(R.path([KEY_PROUTER, KEY_ADDRESS]))
-    .then(
-      rejectIfNil(
-        `Could not find any ${KEY_PROUTER} address for '${hre.network.name}' network, is it deployed?`
-      )
-    )
-    .then(_address => console.info(`Found ${KEY_PROUTER} @ ${_address}`) || _address)
-
-const getStateManagerAddress = (hre, _config) =>
-  Promise.resolve(_config.get(hre.network.name))
-    .then(R.path([KEY_STATEMANAGER, KEY_ADDRESS]))
-    .then(
-      rejectIfNil(
-        `Could not find any ${KEY_STATEMANAGER} address for '${hre.network.name}' network, is it deployed?`
-      )
-    )
-    .then(_address => console.info(`Found ${KEY_STATEMANAGER} @ ${_address}`) || _address)
-
 const isAssetAddressEqualTo = _address => R.compose(R.equals(_address), R.prop(KEY_ADDRESS))
 
-const getUnderlyingAsset = (_chainName, _config, _underlyingAssetAddress) =>
-  Promise.resolve(_config.get(_chainName))
+const changeHardhatNetworkAndReturnArg = R.curry((hre, _chainName, _arg) =>
+  Promise.resolve(hre.changeNetwork(_chainName))
+    .then(_ => _arg)
+)
+
+const getUnderlyingAsset = (taskArgs, hre) =>
+  new Promise(resolve => {
+  const underlyingAssetAddress = taskArgs[TASK_PARAM_UNDERLYING_ASSET_ADDRESS]
+  const underlyingAssetChainName = taskArgs[TASK_PARAM_UNDERLYING_ASSET_CHAIN_NAME]
+  const selectedChain = hre.network.name
+
+  return getConfiguration()
+    .then(_config => _config.get(taskArgs[TASK_PARAM_UNDERLYING_ASSET_CHAIN_NAME]))
     .then(R.prop(KEY_UNDERLYING_ASSET_LIST))
-    .then(R.filter(isAssetAddressEqualTo(_underlyingAssetAddress)))
+    .then(R.filter(isAssetAddressEqualTo(underlyingAssetAddress)))
     .then(
-      rejectIfLength0(`Could not find any underlying asset with address ${_underlyingAssetAddress}`)
+      rejectIfLength0(`Could not find any underlying asset with address ${underlyingAssetAddress}`)
     )
     .then(R.prop(0))
     .then(
       _underlyingAsset =>
-        console.info(`Underlying asset for ${_underlyingAssetAddress} found!`) || _underlyingAsset
+        console.info(`Underlying asset for ${underlyingAssetAddress} found!`) || _underlyingAsset
+    )
+    .then(changeHardhatNetworkAndReturnArg(hre, underlyingAssetChainName))
+    .then(attachToUnderlyingAsset(taskArgs, hre))
+    .then(changeHardhatNetworkAndReturnArg(hre, selectedChain))
+    .then(resolve)
+})
+
+const getUnderlyingAssetNetworkId = (taskArgs, hre) =>
+  getConfiguration()
+    .then(_config => _config.get(taskArgs[TASK_PARAM_UNDERLYING_ASSET_CHAIN_NAME]))
+    .then(R.prop(KEY_NETWORK_ID))
+
+const attachToContract = R.curry(
+  (hre, _contractName, _address) =>
+    hre.ethers.getContractFactory(_contractName).then(_factory => _factory.attach(_address))
+)
+
+const getFactoryContract = (taskArgs, hre) =>
+  getConfiguration()
+    .then(_config => _config.get(taskArgs[TASK_PARAM_UNDERLYING_ASSET_CHAIN_NAME]))
+    .then(R.path([KEY_PFACTORY, KEY_ADDRESS]))
+    .then(attachToContract(hre, CONTRACT_NAME_PFACTORY))
+    .catch(_err =>
+      _err.message.includes('invalid contract address')
+        ? Promise.reject(new Error(`Can't find any PFactory contract for '${hre.network.name}', have you deployed it?`))
+        : Promise.reject(_err)
     )
 
-const deployPTokenTask = (taskArgs, hre) =>
+const checkStateManagerIsDeployed = (taskArgs, hre) =>
   getConfiguration()
-    .then(_config =>
-      Promise.all([
-        getPFactoryAddress(hre, _config),
-        getPRouterAddress(hre, _config),
-        getStateManagerAddress(hre, _config),
-        _config.get(taskArgs.underlyingChain)[KEY_NETWORK_ID],
-        getUnderlyingAsset(taskArgs.underlyingChain, _config, taskArgs.underlyingAssetAddress),
-      ])
-    )
-    .then(
-      ([
-        _pFactoryAddress,
-        _pRouterAddress,
-        _pStateManagerAddress,
-        _underlyingAssetNetworkId,
-        _underlyingAsset,
-      ]) =>
-        hre.run(TASK_NAME_DEPLOY_ASSET, {
-          configurableName: KEY_PTOKEN_LIST,
-          contractFactoryName: CONTRACT_NAME_PTOKEN,
-          overrides: { gasLimit: taskArgs.gas, gasPrice: taskArgs.gasPrice },
-          deployArgsArray: [
-            _underlyingAsset.name,
-            _underlyingAsset.symbol,
-            _underlyingAsset.decimals,
-            _underlyingAsset.address,
-            _underlyingAssetNetworkId,
-            _pRouterAddress,
-            _pStateManagerAddress,
-          ],
-        })
+    .then(_config => _config.get(hre.network.name))
+    .then(R.path([KEY_STATEMANAGER, KEY_ADDRESS]))
+    .then(rejectIfNil(`Could not find any StateManager address for '${hre.network.name}', have you deployed it?`))
+
+const checkPRouterIsDeployed = (taskArgs, hre) =>
+  getConfiguration()
+    .then(_config => _config.get(hre.network.name))
+    .then(R.path([KEY_PROUTER, KEY_ADDRESS]))
+    .then(rejectIfNil(`Could not find any PRouter address for '${hre.network.name}', have you deployed it?`))
+
+const getPTokenDeployArgs = (taskArgs, hre) =>
+  Promise.all([
+    getUnderlyingAsset(taskArgs, hre),
+    getUnderlyingAssetNetworkId(taskArgs, hre),
+    getFactoryContract(taskArgs, hre),
+  ])
+  .then(
+    ([
+      _underlyingAssetContract,
+      _underlyingAssetNetworkId,
+      pFactory,
+    ]) => console.log(_underlyingAssetContract) ||Promise.all([
+      _underlyingAssetContract.name(),
+      _underlyingAssetContract.symbol(),
+      _underlyingAssetContract.decimals(),
+      _underlyingAssetContract.address,
+      _underlyingAssetNetworkId,
+      { pFactory },
+    ])
+  )
+
+const maybeOverwriteParamsWithDefaultValues = (taskArgs, hre) =>
+  Promise.resolve(
+    R.isNil(taskArgs[TASK_PARAM_UNDERLYING_ASSET_CHAIN_NAME])
+      ? R.assoc(TASK_PARAM_UNDERLYING_ASSET_CHAIN_NAME, hre.network.name, taskArgs)
+      : taskArgs
+  )
+
+const deployPTokenTask = (taskArgs, hre) =>
+  checkPRouterIsDeployed(taskArgs, hre)
+    .then(_ => checkStateManagerIsDeployed(taskArgs, hre))
+    .then(_ => maybeOverwriteParamsWithDefaultValues(taskArgs, hre))
+    .then(_taskArgs => getPTokenDeployArgs(_taskArgs, hre))
+    .then(_args => deployPToken(..._args))
+    .catch(_err =>
+      _err.message.includes('cannot estimate gas')
+        ? console.error('Error: gas estimation failed, are you sure the pToken is not deployed already? If yes, try to overwrite the gas options.')
+        : Promise.reject(_err)
     )
 
 task(TASK_NAME_DEPLOY_PTOKEN, TASK_DESC_DEPLOY_PTOKEN, deployPTokenTask)
   .addPositionalParam(
-    'underlyingAssetAddress',
+    TASK_PARAM_UNDERLYING_ASSET_ADDRESS,
     'Underlying asset asset address we want to wrap',
     undefined,
     types.string
   )
-  .addPositionalParam('underlyingChain', 'Underlying Asset chain name', undefined, types.string)
-  .addOptionalParam('gas', 'Optional gas limit setting', undefined, types.int)
-  .addOptionalParam('gasPrice', 'Optional gas price setting', undefined, types.int)
+  .addOptionalParam(
+    TASK_PARAM_UNDERLYING_ASSET_CHAIN_NAME,
+    'Underlying Asset chain name (defaults to the selected network)',
+    undefined,
+    types.string
+  )
+  .addOptionalParam(TASK_PARAM_GAS, 'Optional gas limit setting', undefined, types.int)
+  .addOptionalParam(TASK_PARAM_GASPRICE, 'Optional gas price setting', undefined, types.int)
