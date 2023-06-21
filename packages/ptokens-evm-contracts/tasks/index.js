d592508dc21676760c27adcc720fcf1dc9c36c01
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-06-21 14:56:03 +0100

    fix(ptokens-evm-contracts): export

--- packages/ptokens-evm-contracts/tasks/index.js
+++ packages/ptokens-evm-contracts/tasks/index.js
@@ -3,11 +3,13 @@ const erc20 = require('./erc20')
 const deploy = require('./deploy')
 const getNetworkId = require('./get-network-id')
 const userOperations = require('./user-operations')
+const stateManager = require('./state-manager')
 
 module.exports = {
   apps,
   erc20,
   deploy,
   getNetworkId,
+  stateManager,
   userOperations,
 }
