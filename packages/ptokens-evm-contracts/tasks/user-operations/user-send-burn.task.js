e0451e38e93e972c2439db1ef116054fc296f7c8
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-06-21 12:00:22 +0100

    fix(ptokens-evm-contracts): use `PToken.burn()` instead

--- packages/ptokens-evm-contracts/tasks/user-operations/user-send-burn.task.js
+++ packages/ptokens-evm-contracts/tasks/user-operations/user-send-burn.task.js
@@ -4,9 +4,7 @@ const R = require('ramda')
 
 const {
   KEY_ADDRESS,
-  KEY_PROUTER,
   KEY_PTOKEN_LIST,
-  KEY_NETWORK_ID,
   KEY_PTOKEN_UNDERLYING_ASSET_ADDRESS,
   KEY_PTOKEN_UNDERLYING_ASSET_NETWORKID,
   PARAM_NAME_PTOKEN_ADDRESS,
@@ -29,52 +27,26 @@ const getAssetFromPToken = (pTokenAddress, config, hre) => {
 const burn = async (taskArgs, hre) => {
   const config = await getConfiguration()
   const signer = await hre.ethers.getSigner()
-  const PRouter = await hre.ethers.getContractFactory('PRouter')
-  const pRouter = await PRouter.attach(config.get(hre.network.name)[KEY_PROUTER][KEY_ADDRESS])
 
-  const [underlyingAssetAddress, underlyingAssetChainName] = getAssetFromPToken(
-    taskArgs.pTokenAddress,
-    config,
-    hre
-  )
+  const [underlyingAssetAddress] = getAssetFromPToken(taskArgs.pTokenAddress, config, hre)
 
-  const currentChain = hre.network.name
-  hre.changeNetwork(underlyingAssetChainName)
   const ERC20 = await hre.ethers.getContractFactory('ERC20')
   const asset = await ERC20.attach(underlyingAssetAddress)
-  const underlyingAssetName = await asset.name()
   const underlyingAssetSymbol = await asset.symbol()
-  const underlyingAssetDecimals = await asset.decimals()
-  hre.changeNetwork(currentChain)
 
-  const destinationNetworkId = config.get(hre.network.name)[KEY_NETWORK_ID]
-  const underlyingAssetNetworkId = config.get(underlyingAssetChainName)[KEY_NETWORK_ID]
+  const pTokenFactory = await hre.ethers.getContractFactory('PToken')
+  const ptoken = await pTokenFactory.attach(taskArgs.pTokenAddress)
+  const ptokenSymbol = await ptoken.symbol()
 
   console.info(
     `${taskArgs.amount} ptokens ${taskArgs.pTokenAddress} will be burned in order to reedem ${taskArgs.amount} ${underlyingAssetSymbol} (address: ${underlyingAssetAddress})`
   )
   console.info(`Account requesting the reedem: ${signer.address}`)
   const parsedAmount = hre.ethers.utils.parseEther(taskArgs.amount)
-  console.info(`Approving ${taskArgs.amount} ptokens from ${taskArgs.pTokenAddress}`) // TODO check if there is a ptoken symbol
-  await asset.approve(taskArgs.pTokenAddress, parsedAmount)
-  console.info('Generating an UserOperation ...')
+  await asset.approve(signer.address, parsedAmount)
+  console.info(`Redeeming ${taskArgs.amount} ${ptokenSymbol} to address ${signer.address}`)
+  const tx = await ptoken.burn(parsedAmount)
 
-  const tx = await pRouter.userSend(
-    signer.address,
-    destinationNetworkId,
-    underlyingAssetName,
-    underlyingAssetSymbol,
-    underlyingAssetDecimals,
-    underlyingAssetAddress,
-    underlyingAssetNetworkId,
-    taskArgs.pTokenAddress,
-    parsedAmount,
-    '0x',
-    '0x'.padEnd(66, '0'),
-    {
-      gasLimit: 200000,
-    }
-  )
   await tx.wait(1)
 }
 
