198558fa0fe29f09390b4ab816e282d8b8fbae7e
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-06-27 17:29:00 +0100

    chore(ptokens-evm-contracts): generate also the Guardian `config.json` files

---
+++ packages/ptokens-evm-contracts/tasks/apps/generate-configs.task.js
@@ -0,0 +1,166 @@
+const R = require('ramda')
+const path = require('path')
+const fs = require('node:fs/promises')
+const constants = require('ptokens-constants')
+const {
+  getNetworkId,
+  getPRouterAddress,
+  getStateManagerAddress,
+} = require('../lib/configuration-manager')
+
+const TASK_FLAG_SHOW = 'show'
+const TASK_FLAG_SHOW_DESC = 'Show result instead of saving it to a file'
+const TASK_FLAG_MONGO_LOCALHOST = 'localhost'
+const TASK_FLAG_MONGO_LOCALHOST_DESC = 'Set localhost into the mongo url (good for testing)'
+const TASK_NAME_APPS_GENERATE_CONFIGURATIONS = 'apps:generate-configs'
+const TASK_DESC_APPS_GENERATE_CONFIGURATIONS =
+  'Generate Relayer & Guardian configurations from the given network name.'
+
+const PATH_TO_RELAYER_APP = path.join(__dirname, '../../../../apps/ptokens-relayer')
+const PATH_TO_GUARDIAN_APP = path.join(__dirname, '../../../../apps/ptokens-guardian')
+
+const prettyStringify = _object => JSON.stringify(_object, null, 2)
+
+const saveConfiguration = R.curry((_what, _path, _configuration) =>
+  fs
+    .writeFile(_path, prettyStringify(_configuration))
+    .then(_ => console.info(`${_what} configuration saved to ${_path}`))
+)
+
+const showConfiguration = (_what, _configuration) =>
+  console.info(`# ${_what} configuration`) || console.info(prettyStringify(_configuration))
+
+const maybeSaveConfiguration = R.curry((taskArgs, _what, _path, _configuration) =>
+  taskArgs[TASK_FLAG_SHOW]
+    ? showConfiguration(_what, _configuration)
+    : saveConfiguration(_what, _path, _configuration)
+)
+
+const getMongoUrlFromTaskArgs = taskArgs =>
+  taskArgs[TASK_FLAG_MONGO_LOCALHOST] ? 'mongodb://localhost:27017' : 'mongodb://mongodb:27017'
+
+const generateListenerConfiguration = (
+  taskArgs,
+  hre,
+  _networkId,
+  _contractAddress,
+  _eventSignature
+) =>
+  Promise.resolve({
+    [constants.config.KEY_CHAIN_TYPE]: 'EVM',
+    [constants.config.KEY_NETWORK_ID]: _networkId,
+    [constants.config.KEY_CHAIN_NAME]: hre.network.name,
+    [constants.config.KEY_PROVIDER_URL]: hre.network.config.url,
+    [constants.config.KEY_EVENTS]: [
+      {
+        [constants.config.KEY_CONTRACTS]: [_contractAddress],
+        [constants.config.KEY_NAME]: _eventSignature,
+      },
+    ],
+    [constants.config.KEY_DB]: {
+      [constants.config.KEY_NAME]: 'pnetwork',
+      [constants.config.KEY_TABLE_EVENTS]: 'events',
+      [constants.config.KEY_URL]: getMongoUrlFromTaskArgs(taskArgs),
+    },
+  })
+
+const generateRequestProcessorConfiguration = (taskArgs, hre, _networkId, _contractAddress) =>
+  Promise.all([getStateManagerAddress(hre), getNetworkId(hre)]).then(
+    ([_stateManagerAddress, _networkId]) => ({
+      [constants.config.KEY_CHAIN_TYPE]: 'EVM',
+      [constants.config.KEY_CHALLENGE_PERIOD]: 1, // FIXME
+      [constants.config.KEY_NETWORK_ID]: _networkId,
+      [constants.config.KEY_CHAIN_NAME]: hre.network.name,
+      [constants.config.KEY_STATE_MANAGER]: _contractAddress,
+      [constants.config.KEY_IDENTITY_GPG]: '/usr/src/app/private-key',
+      [constants.config.KEY_PROVIDER_URL]: hre.network.config.url,
+      [constants.config.KEY_DB]: {
+        [constants.config.KEY_NAME]: 'pnetwork',
+        [constants.config.KEY_TABLE_EVENTS]: 'events',
+        [constants.config.KEY_URL]: getMongoUrlFromTaskArgs(taskArgs),
+      },
+    })
+  )
+
+const saveRelayerListenerConfiguration = (taskArgs, hre, _networkId, _pRouterAddress) =>
+  generateListenerConfiguration(
+    taskArgs,
+    hre,
+    _networkId,
+    _pRouterAddress,
+    constants.evm.events.USER_OPERATION_SIGNATURE
+  ).then(
+    maybeSaveConfiguration(
+      taskArgs,
+      'Relayer listener',
+      `${PATH_TO_RELAYER_APP}/${hre.network.name}.listener.config.json`
+    )
+  )
+
+const saveRelayerProcessorConfiguration = (taskArgs, hre, _networkId, _stateManagerAddress) =>
+  generateRequestProcessorConfiguration(taskArgs, hre, _networkId, _stateManagerAddress).then(
+    maybeSaveConfiguration(
+      taskArgs,
+      'Relayer processor',
+      `${PATH_TO_RELAYER_APP}/${hre.network.name}.processor.config.json`
+    )
+  )
+
+const saveGuardianRequestListenerConfiguration = (taskArgs, hre, _networkId, _pRouterAddress) =>
+  generateListenerConfiguration(
+    taskArgs,
+    hre,
+    _networkId,
+    _pRouterAddress,
+    constants.evm.events.USER_OPERATION_SIGNATURE
+  ).then(
+    maybeSaveConfiguration(
+      taskArgs,
+      'Guardian request listener',
+      `${PATH_TO_GUARDIAN_APP}/${hre.network.name}.listener.requests.config.json`
+    )
+  )
+
+const saveGuardianQueueListenerConfiguration = (taskArgs, hre, _networkId, _stateManagerAddress) =>
+  generateListenerConfiguration(
+    taskArgs,
+    hre,
+    _networkId,
+    _stateManagerAddress,
+    constants.evm.events.OPERATION_QUEUED_SIGNATURE
+  ).then(
+    maybeSaveConfiguration(
+      taskArgs,
+      'Guardian queue listener',
+      `${PATH_TO_GUARDIAN_APP}/${hre.network.name}.listener.queue.config.json`
+    )
+  )
+
+const saveGuardianProcessorConfiguration = (taskArgs, hre, _networkId, _stateManagerAddress) =>
+  generateRequestProcessorConfiguration(taskArgs, hre, _networkId, _stateManagerAddress).then(
+    maybeSaveConfiguration(
+      taskArgs,
+      'Guardian processor',
+      `${PATH_TO_GUARDIAN_APP}/${hre.network.name}.processor.config.json`
+    )
+  )
+
+const generateRelayerConfigurationTask = (taskArgs, hre) =>
+  Promise.all([getNetworkId(hre), getPRouterAddress(hre), getStateManagerAddress(hre)]).then(
+    ([_networkId, _pRouterAddress, _stateManagerAddress]) =>
+      Promise.all([
+        saveRelayerListenerConfiguration(taskArgs, hre, _networkId, _pRouterAddress),
+        saveRelayerProcessorConfiguration(taskArgs, hre, _networkId, _stateManagerAddress),
+        saveGuardianRequestListenerConfiguration(taskArgs, hre, _networkId, _pRouterAddress),
+        saveGuardianQueueListenerConfiguration(taskArgs, hre, _networkId, _stateManagerAddress),
+        saveGuardianProcessorConfiguration(taskArgs, hre, _networkId, _stateManagerAddress),
+      ])
+  )
+
+task(
+  TASK_NAME_APPS_GENERATE_CONFIGURATIONS,
+  TASK_DESC_APPS_GENERATE_CONFIGURATIONS,
+  generateRelayerConfigurationTask
+)
+  .addFlag(TASK_FLAG_SHOW, TASK_FLAG_SHOW_DESC)
+  .addFlag(TASK_FLAG_MONGO_LOCALHOST, TASK_FLAG_MONGO_LOCALHOST_DESC)
