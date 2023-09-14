e862e8d64255b45f15b40b1f889a8fff5deeac54
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-09-14 10:55:47 +0200

    refactor(ptokens-evm-contracts): use private variable conventions

--- packages/ptokens-evm-contracts/contracts/core/PRegistry.sol
+++ packages/ptokens-evm-contracts/contracts/core/PRegistry.sol
@@ -7,23 +7,23 @@ import {IPRegistry} from "../interfaces/IPRegistry.sol";
 contract PRegistry is IPRegistry, AccessControl {
   bytes32 public constant ADD_SUPPORTED_NETWORK_ID_ROLE = keccak256("ADD_SUPPORTED_NETWORK_ID_ROLE");
 
-  mapping(bytes4 => address) private networkIdToHub_;
+  mapping(bytes4 => address) private _networkIdToHub;
 
   constructor(address dao) {
     _setupRole(ADD_SUPPORTED_NETWORK_ID_ROLE, dao);
   }
 
   function isNetworkIdSupported(bytes4 networkId) external view returns (bool) {
-    address hub = networkIdToHub_[networkId];
+    address hub = _networkIdToHub[networkId];
 
     return (hub != address(0));
   }
 
   function addSupportedNetworkId(bytes4 networkId, address hub) public onlyRole(ADD_SUPPORTED_NETWORK_ID_ROLE) {
-    networkIdToHub_[networkId] = hub;
+    _networkIdToHub[networkId] = hub;
   }
 
   function hubByNetworkId(bytes4 sourceNetworkId) external view returns (address) {
-    return networkIdToHub_[sourceNetworkId];
+    return _networkIdToHub[sourceNetworkId];
   }
 }
