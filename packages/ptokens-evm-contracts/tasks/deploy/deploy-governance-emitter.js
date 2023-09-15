c090a7da3599db1762e15e1e360ca1a6ba898d88
Alain Olivier <oliviera9@gmail.com>
2023-09-15 15:10:57 +0200

    fix(ptokens-evm-contracts): add required parameters for GovernanceMessageEmitter deploy script

--- packages/ptokens-evm-contracts/tasks/deploy/deploy-governance-emitter.js
+++ packages/ptokens-evm-contracts/tasks/deploy/deploy-governance-emitter.js
@@ -1,5 +1,25 @@
-task('deploy-governance-emitter', 'Deploy a GovernanceMessageEmitter contract').setAction(
-  async _args => {
+const { types } = require('hardhat/config')
+
+task('deploy-governance-emitter', 'Deploy a GovernanceMessageEmitter contract')
+  .addPositionalParam(
+    'epochsManagerAddress',
+    'Epochs Manager Contract Address',
+    undefined,
+    types.string
+  )
+  .addPositionalParam(
+    'lendingManagerAddress',
+    'Lending Manager Contract Address',
+    undefined,
+    types.string
+  )
+  .addPositionalParam(
+    'registrationManagerAddress',
+    'Registration Manager Contract Address',
+    undefined,
+    types.string
+  )
+  .setAction(async _args => {
     await main(_args)
       // eslint-disable-next-line no-process-exit
       .then(() => process.exit(0))
@@ -8,15 +28,18 @@ task('deploy-governance-emitter', 'Deploy a GovernanceMessageEmitter contract').
         // eslint-disable-next-line no-process-exit
         process.exit(1)
       })
-  }
-)
+  })
 
 /* eslint-disable no-console */
-const main = async () => {
+const main = async _args => {
   const GovernanceMessageEmitter = await ethers.getContractFactory('GovernanceMessageEmitter')
 
   console.log('Deploying GovernanceMessageEmitter ...')
-  const governanceMessageEmitter = await GovernanceMessageEmitter.deploy()
+  const governanceMessageEmitter = await GovernanceMessageEmitter.deploy(
+    _args.epochsManagerAddress,
+    _args.lendingManagerAddress,
+    _args.registrationManagerAddress
+  )
 
   console.log({
     governanceMessageEmitter: governanceMessageEmitter.address,
