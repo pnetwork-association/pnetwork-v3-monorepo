7f21e9b398e7990c5f87fd2fe487097e31d633db
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-09-18 12:35:54 +0200

    feat(ptokens-state-emitter): compute EVM signature

---
+++ packages/ptokens-state-emitter/test/sign-state-object.test.js
@@ -0,0 +1,30 @@
+const assert = require('assert')
+const ethers = require('ethers')
+
+const { utils } = require('ptokens-utils')
+const stateObject = require('./samples/state-object')
+const {
+  signStateObject,
+  KEY_SIGNATURE,
+  KEY_SIGNER_ADDRESS,
+} = require('../lib/interfaces/sign-state-object')
+const mockGuardianIdentity = require('./samples/mock-guardian-identity')
+
+describe('Signature tests', () => {
+  it('Should sign the state object successfully', async () => {
+    const wallet = new ethers.Wallet(mockGuardianIdentity.privateKey)
+    const result = await signStateObject(wallet, stateObject)
+
+    const expectedSignature =
+      '0xee4b9501c71d5a4e3a3ce7462ba595f74ace879b9be2992aadee11cbebff6b615223a34c33e3fcde159424e32f5a95d796e3ba1b2dd71109ebe6ad48ecad2d951b'
+
+    assert.deepStrictEqual(result[KEY_SIGNATURE], expectedSignature)
+
+    const stateObjectSorted = utils.sortKeysAlphabetically(stateObject)
+    const expectedMessage = JSON.stringify(stateObjectSorted)
+
+    const signerAddress = await ethers.verifyMessage(expectedMessage, result[KEY_SIGNATURE])
+
+    assert.deepStrictEqual(signerAddress, stateObject[KEY_SIGNER_ADDRESS])
+  })
+})
