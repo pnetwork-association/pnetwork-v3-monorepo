75effa695b473d57ab618473882c326a2bd9f172
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-09-18 14:36:14 +0200

    feat(ptokens-utils): add `sortKeysAlphabetically` (`utils-ramda-ext.js`)

--- packages/ptokens-utils/test/utils/utils-ramda-ext.test.js
+++ packages/ptokens-utils/test/utils/utils-ramda-ext.test.js
@@ -13,4 +13,50 @@ describe('General ramda extensions testing', () => {
       assert.deepStrictEqual(result, expected)
     })
   })
+
+  describe('sortKeysAlphabetically', () => {
+    it('Should sort the keys alphabetically of an object', async () => {
+      const expected = {
+        a: 1,
+        b: [1, 2, 3],
+        c: {
+          d: 1,
+          e: {
+            f: [7, 8, 9],
+            g: [4, 5, 6],
+          },
+          h: {
+            i: {
+              l: 1,
+              m: 2,
+              o: 3,
+            },
+          },
+        },
+      }
+
+      const permutedObject = {
+        c: {
+          e: {
+            g: [4, 5, 6],
+            f: [7, 8, 9],
+          },
+          h: {
+            i: {
+              o: 3,
+              l: 1,
+              m: 2,
+            },
+          },
+          d: 1,
+        },
+        b: [1, 2, 3],
+        a: 1,
+      }
+
+      const result = utils.sortKeysAlphabetically(permutedObject)
+
+      assert.deepStrictEqual(result, expected)
+    })
+  })
 })
