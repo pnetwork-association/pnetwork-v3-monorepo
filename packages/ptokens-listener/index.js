18fbf7a325571ffeb0c036707c63c22adceb68da
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-09-21 12:21:41 +0200

    chore(ptokens-listener): add new command for getting the `GuardiansPropagated` db report (index.js)

--- packages/ptokens-listener/index.js
+++ packages/ptokens-listener/index.js
@@ -20,6 +20,14 @@ const disableLoggingForCLICommand = _ => {
   utilsLogger.logger.level = 'off'
 }
 
+const GET_OPERATIONS_CMD = 'getOperations'
+const GET_USER_OPERATION_CMD = 'getUserOperation'
+const GET_OPERATION_QUEUED_CMD = 'getOperationQueued'
+const GET_OPERATION_EXECUTED_CMD = 'getOperationExecuted'
+const GET_GUARDIANS_PROPAGATED_CMD = 'getGuardiansPropagated'
+const GET_EVENT_LOGS_FROM_TRANSACTION_CMD = 'getEventsLogsFromTransaction'
+const GET_EVENT_REPORTS_FROM_TRANSACTION_CMD = 'getEventReportsFromTransaction'
+
 const EXAMPLE_CALLS = `
 Example calls:
 
@@ -27,34 +35,39 @@ Example calls:
 
 const GET_EVENT_REPORTS_FROM_TRANSACTION_HELP_MESSAGE =
   EXAMPLE_CALLS +
-  `$ node index.js getEventReportsFromTransaction 0x2d300f8aeed6cee69f50dde84d0a6e991d0836b2a1a3b3a6737b3ae3493f710f 'UserOperation(uint256 nonce, string destinationAccount, bytes4 destinationNetworkId, string underlyingAssetName, string underlyingAssetSymbol, uint256 underlyingAssetDecimals, address underlyingAssetTokenAddress, bytes4 underlyingAssetNetworkId, address assetTokenAddress, uint256 assetAmount, address protocolFeeAssetTokenAddress, uint256 protocolFeeAssetAmount, uint256 networkFeeAssetAmount, uint256 forwardNetworkFeeAssetAmount, bytes4 forwardDestinationNetworkId, bytes userData, bytes32 optionsMask)'
+  `$ node index.js ${GET_EVENT_REPORTS_FROM_TRANSACTION_CMD} 0x2d300f8aeed6cee69f50dde84d0a6e991d0836b2a1a3b3a6737b3ae3493f710f 'UserOperation(uint256 nonce, string destinationAccount, bytes4 destinationNetworkId, string underlyingAssetName, string underlyingAssetSymbol, uint256 underlyingAssetDecimals, address underlyingAssetTokenAddress, bytes4 underlyingAssetNetworkId, address assetTokenAddress, uint256 assetAmount, address protocolFeeAssetTokenAddress, uint256 protocolFeeAssetAmount, uint256 networkFeeAssetAmount, uint256 forwardNetworkFeeAssetAmount, bytes4 forwardDestinationNetworkId, bytes userData, bytes32 optionsMask)'
 `
 
 const GET_EVENT_LOGS_FROM_TRANSACTION_HELP_MESSAGE =
   EXAMPLE_CALLS +
-  `$ node index.js getEventLogsFromTransaction 0x2d300f8aeed6cee69f50dde84d0a6e991d0836b2a1a3b3a6737b3ae3493f710f
+  `$ node index.js ${GET_EVENT_LOGS_FROM_TRANSACTION_CMD} 0x2d300f8aeed6cee69f50dde84d0a6e991d0836b2a1a3b3a6737b3ae3493f710f
 
-$ node index.js getEventLogsFromTransaction 0x2d300f8aeed6cee69f50dde84d0a6e991d0836b2a1a3b3a6737b3ae3493f710f 'UserOperation(uint256 nonce, string destinationAccount, bytes4 destinationNetworkId, string underlyingAssetName, string underlyingAssetSymbol, uint256 underlyingAssetDecimals, address underlyingAssetTokenAddress, bytes4 underlyingAssetNetworkId, address assetTokenAddress, uint256 assetAmount, address protocolFeeAssetTokenAddress, uint256 protocolFeeAssetAmount, uint256 networkFeeAssetAmount, uint256 forwardNetworkFeeAssetAmount, bytes4 forwardDestinationNetworkId, bytes userData, bytes32 optionsMask)'
+$ node index.js ${GET_EVENT_LOGS_FROM_TRANSACTION_CMD} 0x2d300f8aeed6cee69f50dde84d0a6e991d0836b2a1a3b3a6737b3ae3493f710f 'UserOperation(uint256 nonce, string destinationAccount, bytes4 destinationNetworkId, string underlyingAssetName, string underlyingAssetSymbol, uint256 underlyingAssetDecimals, address underlyingAssetTokenAddress, bytes4 underlyingAssetNetworkId, address assetTokenAddress, uint256 assetAmount, address protocolFeeAssetTokenAddress, uint256 protocolFeeAssetAmount, uint256 networkFeeAssetAmount, uint256 forwardNetworkFeeAssetAmount, bytes4 forwardDestinationNetworkId, bytes userData, bytes32 optionsMask)'
+`
+
+const GET_GUARDIANS_PROPAGATED_HELP_MESSAGE =
+  EXAMPLE_CALLS +
+  `$ node index.js ${GET_GUARDIANS_PROPAGATED_CMD} 0xFf310f8aeed6cee69f50dde84d0a6e991d0836b2a1a3b3a6737b3ae3493f710f
 `
 
 const GET_USER_OPERATION_HELP_MESSAGE =
   EXAMPLE_CALLS +
-  `$ node index.js getUserOperation 0x2d300f8aeed6cee69f50dde84d0a6e991d0836b2a1a3b3a6737b3ae3493f710f
+  `$ node index.js ${GET_USER_OPERATION_CMD} 0x2d300f8aeed6cee69f50dde84d0a6e991d0836b2a1a3b3a6737b3ae3493f710f
 `
 
 const GET_OPERATION_QUEUED_HELP_MESSAGE =
   EXAMPLE_CALLS +
-  `$ node index.js getOperationQueued 0x261229b0af24a5caaf24edc96a0e4ccafa801ef873ab4dff2277538232b38e79
+  `$ node index.js ${GET_OPERATION_QUEUED_CMD} 0x261229b0af24a5caaf24edc96a0e4ccafa801ef873ab4dff2277538232b38e79
 `
 
 const GET_OPERATION_EXECUTED_HELP_MESSAGE =
   EXAMPLE_CALLS +
-  `$ node index.js getOperationExecuted 0x1091be7256f91c7025906b4cd82332e3b7d671c8ef60df08c14dc06fa11cf49a
+  `$ node index.js ${GET_OPERATION_EXECUTED_CMD} 0x1091be7256f91c7025906b4cd82332e3b7d671c8ef60df08c14dc06fa11cf49a
 `
 
 const GET_OPERATIONS_HELP_MESSAGE =
   EXAMPLE_CALLS +
-  `$ node index.js getOperations 0x46840d7667c567d8ae702801c296d9cb19535d7c77f8e132c79f06c25df79600 0x565033350582f4Ad298fDD8d59b7c36D0cAC1712 --fromBlock 34923840
+  `$ node index.js ${GET_OPERATIONS_CMD} 0x46840d7667c567d8ae702801c296d9cb19535d7c77f8e132c79f06c25df79600 0x565033350582f4Ad298fDD8d59b7c36D0cAC1712 --fromBlock 34923840
 `
 
 const addMainCommand = _program =>
@@ -66,7 +79,7 @@ const addMainCommand = _program =>
 
 const addGetEventReportsFromTransactionCommand = _program =>
   _program
-    .command('getEventReportsFromTransaction')
+    .command(GET_EVENT_REPORTS_FROM_TRANSACTION_CMD)
     .description('Get event reports in a specific transaction')
     .argument('<tx–hash>', 'transaction hash')
     .argument('<event-signature>', 'event signature')
@@ -80,7 +93,7 @@ const addGetEventReportsFromTransactionCommand = _program =>
 
 const addGetEventLogsFromTransactionCommand = _program =>
   _program
-    .command('getEventLogsFromTransaction')
+    .command(GET_EVENT_LOGS_FROM_TRANSACTION_CMD)
     .description('Get event logs for a specific transaction')
     .argument('<tx–hash>', 'transaction hash')
     .argument('[event-signature]', 'event signature', null)
@@ -92,7 +105,7 @@ const addGetEventLogsFromTransactionCommand = _program =>
 
 const addGetUserOperationCommand = _program =>
   _program
-    .command('getUserOperation')
+    .command(GET_USER_OPERATION_CMD)
     .description('Get UserOperation event reports in a specific transaction')
     .argument('<tx–hash>', 'transaction hash')
     .option('-s, --save', 'save report into database')
@@ -110,7 +123,7 @@ const addGetUserOperationCommand = _program =>
 
 const addGetOperationQueuedCommand = _program =>
   _program
-    .command('getOperationQueued')
+    .command(GET_OPERATION_QUEUED_CMD)
     .description('Get OperationQueued event reports in a specific transaction')
     .argument('<tx–hash>', 'transaction hash')
     .option('-s, --save', 'save report into database')
@@ -128,7 +141,7 @@ const addGetOperationQueuedCommand = _program =>
 
 const addGetOperationExecutedCommand = _program =>
   _program
-    .command('getOperationExecuted')
+    .command(GET_OPERATION_EXECUTED_CMD)
     .description('Get OperationExecuted event reports in a specific transaction')
     .argument('<tx–hash>', 'transaction hash')
     .option('-s, --save', 'save report into database')
@@ -144,9 +157,27 @@ const addGetOperationExecutedCommand = _program =>
         )
     ) && _program
 
+const addGetGuardiansPropagatedCommand = _program =>
+  _program
+    .command(GET_GUARDIANS_PROPAGATED_CMD)
+    .description('Get OperationExecuted event reports in a specific transaction')
+    .argument('<tx–hash>', 'transaction hash')
+    .option('-s, --save', 'save report into database')
+    .addHelpText('after', GET_GUARDIANS_PROPAGATED_HELP_MESSAGE)
+    .action(
+      (_hash, _options) =>
+        disableLoggingForCLICommand() ||
+        getEventReportsFromTransactionCommand(
+          config,
+          _hash,
+          constants.evm.events.GUADIANS_PROPAGATED_SIGNATURE,
+          _options.save
+        )
+    ) && _program
+
 const addGetOperationsCommand = _program =>
   _program
-    .command('getOperations')
+    .command(GET_OPERATIONS_CMD)
     .description('Get operations linked to an Operation ID')
     .argument('<operationId>', 'operation ID')
     .argument('<state-manager-address>', 'state manager address')
@@ -166,6 +197,7 @@ const main = () =>
     .then(addGetUserOperationCommand)
     .then(addGetOperationQueuedCommand)
     .then(addGetOperationExecutedCommand)
+    .then(addGetGuardiansPropagatedCommand)
     .then(addGetOperationsCommand)
     .then(_program => _program.parseAsync(process.argv))
     .catch(printErrorAndExit)
