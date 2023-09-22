3db691959043d101823958bf0a444ffc0d85cea5
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-09-22 17:42:56 +0200

    chore(ptokens-request-processor): align operation args to the new contract abi

--- packages/ptokens-request-processor/lib/evm/evm-abi-manager.js
+++ packages/ptokens-request-processor/lib/evm/evm-abi-manager.js
@@ -1,92 +1,14 @@
-const R = require('ramda')
 const { logger } = require('../get-logger')
 const constants = require('ptokens-constants')
+const PNetworkHubAbi = require('./abi/PNetworkHub').abi
 
-const userOperationTuple =
-  'tuple(' +
-  'bytes32 originBlockHash, ' +
-  'bytes32 originTransactionHash, ' +
-  'bytes32 optionsMask, ' +
-  'uint256 nonce, ' +
-  'uint256 underlyingAssetDecimals, ' +
-  'uint256 assetAmount, ' +
-  'uint256 protocolFeeAssetAmount, ' +
-  'uint256 networkFeeAssetAmount, ' +
-  'uint256 forwardNetworkFeeAssetAmount, ' +
-  'address underlyingAssetTokenAddress, ' +
-  'bytes4 originNetworkId, ' +
-  'bytes4 destinationNetworkId, ' +
-  'bytes4 forwardDestinationNetworkId, ' +
-  'bytes4 underlyingAssetNetworkId, ' +
-  'string destinationAccount, ' +
-  'string underlyingAssetName, ' +
-  'string underlyingAssetSymbol, ' +
-  'bytes userData, ' +
-  ')'
+const getProtocolOperationAbi = () => PNetworkHubAbi
 
-const hubErrors = [
-  `error OperationAlreadyQueued(${userOperationTuple} operation)`,
-  `error OperationAlreadyExecuted(${userOperationTuple} operation)`,
-  `error OperationAlreadyCancelled(${userOperationTuple} operation)`,
-  `error OperationCancelled(${userOperationTuple} operation)`,
-  `error OperationNotQueued(${userOperationTuple} operation)`,
-  `error GovernanceOperationAlreadyCancelled(${userOperationTuple} operation)`,
-  `error GuardianOperationAlreadyCancelled(${userOperationTuple} operation)`,
-  `error SentinelOperationAlreadyCancelled(${userOperationTuple} operation)`,
-  'error ChallengePeriodNotTerminated(uint64 startTimestamp, uint64 endTimestamp)',
-  'error ChallengePeriodTerminated(uint64 startTimestamp, uint64 endTimestamp)',
-  'error InvalidUnderlyingAssetName(string underlyingAssetName, string expectedUnderlyingAssetName)',
-  'error InvalidUnderlyingAssetSymbol(string underlyingAssetSymbol, string expectedUnderlyingAssetSymbol)',
-  'error InvalidUnderlyingAssetDecimals(uint256 underlyingAssetDecimals, uint256 expectedUnderlyingAssetDecimals)',
-  'error InvalidAssetParameters(uint256 assetAmount, address assetTokenAddress)',
-  'error SenderIsNotHub()',
-  'error InvalidUserOperation()',
-  'error NoUserOperation()',
-  'error PTokenNotCreated(address pTokenAddress)',
-  'error InvalidNetwork(bytes4 networkId)',
-  'error NotContract(address addr)',
-  'error LockDown()',
-  'error InvalidGovernanceMessageEmitter(address expectedGovernanceMessageEmitter, address governanceMessageEmitter)',
-  'error InvalidTopic(bytes32 expectedTopic, bytes32 topic)',
-  'error InvalidReceiptsRootMerkleProof()',
-  'error InvalidRootHashMerkleProof()',
-  'error InvalidHeaderBlock()',
-  'error NotRouter(address sender, address router)',
-  'error InvalidAmount(uint256 amount, uint256 expectedAmount)',
-  'error InvalidSourceChainId(uint32 sourceChainId, uint32 expectedSourceChainId)',
-  'error InvalidGovernanceMessageVerifier(address governanceMessagerVerifier,address expectedGovernanceMessageVerifier)',
-  'error InvalidSentinelRegistration(bytes1 kind)',
-  'error InvalidGovernanceMessage(bytes message)',
-  'error InvalidLockedAmountChallengePeriod(uint256 lockedAmountChallengePeriod,uint256 expectedLockedAmountChallengePeriod)',
-  'error CallFailed()',
-  'error QueueFull()',
-]
+const getProtocolGuardianCancelOperationAbi = () => PNetworkHubAbi
 
-const getProtocolOperationAbi = _operationName => {
-  const abi = [`function ${_operationName}(${userOperationTuple} calldata operation)`]
-  return R.concat(abi, hubErrors)
-}
+const getOperationStatusOfAbi = () => PNetworkHubAbi
 
-const getProtocolGuardianCancelOperationAbi = () => {
-  const abi = [
-    `function protocolGuardianCancelOperation(${userOperationTuple} calldata operation, bytes32[] calldata proof)`,
-  ]
-  return R.concat(abi, hubErrors)
-}
-
-const getOperationStatusOfAbi = () => {
-  const abi = [
-    `function operationStatusOf(${userOperationTuple} calldata operation) public view returns (bytes1)`,
-  ]
-  return R.concat(abi, hubErrors)
-}
-
-const getChallengePeriodOfAbi = () => {
-  const abi = [
-    `function challengePeriodOf(${userOperationTuple} calldata operation) public view returns (uint64, uint64)`,
-  ]
-  return R.concat(abi, hubErrors)
-}
+const getChallengePeriodOfAbi = () => PNetworkHubAbi
 
 const logUserOperationFromAbiArgs = (_operationName, _args) => {
   logger.info(`function ${_operationName}([`)
@@ -105,27 +27,15 @@ const logUserOperationFromAbiArgs = (_operationName, _args) => {
   logger.info(`  bytes4 forwardDestinationNetworkId' ${_args[0][12]}`)
   logger.info(`  bytes4 underlyingAssetNetworkId' ${_args[0][13]}`)
   logger.info(`  string destinationAccount' ${_args[0][14]}`)
-  logger.info(`  string underlyingAssetName' ${_args[0][15]}`)
-  logger.info(`  string underlyingAssetSymbol' ${_args[0][16]}`)
-  logger.info(`  bytes userData' ${_args[0][17]}`)
+  logger.info(`  string originAccount' ${_args[0][15]}`)
+  logger.info(`  string underlyingAssetName' ${_args[0][16]}`)
+  logger.info(`  string underlyingAssetSymbol' ${_args[0][17]}`)
+  logger.info(`  bytes userData' ${_args[0][18]}`)
+  logger.info(`  bool isForProtocol ${_args[0][19]}`)
   logger.info('])')
 }
 
-const getLockedAmountChallengePeriodAbi = () => [
-  {
-    inputs: [],
-    name: 'lockedAmountChallengePeriod',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-]
+const getLockedAmountChallengePeriodAbi = () => PNetworkHubAbi
 
 const getProtocolQueueOperationAbi = () => getProtocolOperationAbi('protocolQueueOperation')
 const getProtocolExecuteOperationAbi = () => getProtocolOperationAbi('protocolExecuteOperation')
@@ -148,18 +58,23 @@ const getUserOperationAbiArgsFromReport = _eventReport => [
     _eventReport[constants.db.KEY_DESTINATION_NETWORK_ID],
     _eventReport[constants.db.KEY_FORWARD_DESTINATION_NETWORK_ID],
     _eventReport[constants.db.KEY_UNDERLYING_ASSET_NETWORK_ID],
+    _eventReport[constants.db.KEY_ORIGINATING_ADDRESS],
     _eventReport[constants.db.KEY_DESTINATION_ACCOUNT],
     _eventReport[constants.db.KEY_UNDERLYING_ASSET_NAME],
     _eventReport[constants.db.KEY_UNDERLYING_ASSET_SYMBOL],
     _eventReport[constants.db.KEY_USER_DATA],
+    _eventReport[constants.db.KEY_IS_FOR_PROTOCOL],
   ],
 ]
+
+const getSolveChallengeGuardianAbi = () => PNetworkHubAbi
 
 module.exports = {
   getOperationStatusOfAbi,
   getChallengePeriodOfAbi,
   logUserOperationFromAbiArgs,
   getProtocolQueueOperationAbi,
+  getSolveChallengeGuardianAbi,
   getProtocolExecuteOperationAbi,
   getUserOperationAbiArgsFromReport,
   getLockedAmountChallengePeriodAbi,
