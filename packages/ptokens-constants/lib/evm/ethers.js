05d9e5cf05fd44281d78894058eab2a2a7a8284a
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-10-03 14:17:03 +0200

    chore(ptokens-constants): make it compact (ethers.js)

--- packages/ptokens-constants/lib/evm/ethers.js
+++ packages/ptokens-constants/lib/evm/ethers.js
@@ -1,13 +1,7 @@
-const ERROR_ESTIMATE_GAS = 'estimateGas'
-const ERROR_CODE_INSUFFICIENT_FUNDS = 'INSUFFICIENT_FUNDS'
-const KEY_TX_HASH = 'transactionHash'
-const ERROR_REPLACEMENT_UNDERPRICED = 'replacement transaction underpriced'
-const TOPIC_HASH = 'topicHash'
-
 module.exports = {
-  ERROR_ESTIMATE_GAS,
-  ERROR_CODE_INSUFFICIENT_FUNDS,
-  KEY_TX_HASH,
-  ERROR_REPLACEMENT_UNDERPRICED,
-  TOPIC_HASH,
+  TOPIC_HASH: 'topicHash',
+  KEY_TX_HASH: 'transactionHash',
+  ERROR_ESTIMATE_GAS: 'estimateGas',
+  ERROR_CODE_INSUFFICIENT_FUNDS: 'INSUFFICIENT_FUNDS',
+  ERROR_REPLACEMENT_UNDERPRICED: 'replacement transaction underpriced',
 }
