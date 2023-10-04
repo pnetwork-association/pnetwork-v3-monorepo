fb84700314b83f4f408cc8dcda2114baa6b6a329
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-10-04 18:14:12 +0200

    chore(ptokens-constants): use `ACTORS_PROPAGATED`

--- packages/ptokens-constants/lib/db/event-names.js
+++ packages/ptokens-constants/lib/db/event-names.js
@@ -2,6 +2,6 @@ module.exports = {
   USER_OPERATION: 'UserOperation',
   QUEUED_OPERATION: 'OperationQueued',
   EXECUTED_OPERATION: 'OperationExecuted',
-  GUARDIANS_PROPAGATED: 'ActorsPropagated',
+  ACTORS_PROPAGATED: 'ActorsPropagated',
   CHALLENGE_PENDING: 'ChallengePending',
 }
