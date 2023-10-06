89d23a9841b7103cec4e8b0ac4e473dd78e911e0
Alain Olivier <oliviera9@gmail.com>
2023-10-06 16:27:13 +0200

    refactor(ptokens-evm-contracts): remove deprecated read-sentinels-root task

--- packages/ptokens-evm-contracts/tasks/governance-message-relayer/index.js
+++ packages/ptokens-evm-contracts/tasks/governance-message-relayer/index.js
@@ -1,13 +1,11 @@
 const decodeGovernanceMessage = require('./decode-governance-message')
 const handleTelepathy = require('./handle-telepathy')
 const propagateSentinels = require('./propagate-actors.task')
-const readSentinelsRoot = require('./read-sentinels-root.task')
 const verifyMessage = require('./verify-message.task')
 
 module.exports = {
   decodeGovernanceMessage,
   handleTelepathy,
   propagateSentinels,
-  readSentinelsRoot,
   verifyMessage,
 }
