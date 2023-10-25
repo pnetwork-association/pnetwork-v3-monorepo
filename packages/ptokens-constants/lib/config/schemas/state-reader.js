77733e33f1bde71e691f58f7624059f34d24eb43
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-10-25 18:22:29 +0200

    chore(ptokens-constants): add `KEY_CHECK_INACTIVITY_INTERVAL` property to state reader configuration

--- packages/ptokens-constants/lib/config/schemas/state-reader.js
+++ packages/ptokens-constants/lib/config/schemas/state-reader.js
@@ -1,12 +1,19 @@
 const R = require('ramda')
 const stateEmitter = require('./state-emitter')
-const { KEY_WARMUP_TIME, KEY_FIRE_CHALLENGE_THRESHOLD } = require('../constants')
+const {
+  KEY_WARMUP_TIME,
+  KEY_CHECK_INACTIVITY_INTERVAL,
+  KEY_FIRE_CHALLENGE_THRESHOLD,
+} = require('../constants')
 
 module.exports = R.mergeDeepWith(R.concat, stateEmitter, {
   required: [KEY_WARMUP_TIME, KEY_FIRE_CHALLENGE_THRESHOLD],
   properties: {
+    [KEY_CHECK_INACTIVITY_INTERVAL]: {
+      type: ['integer', 'null'],
+    },
     [KEY_WARMUP_TIME]: {
-      type: 'integer',
+      type: ['integer', 'null'],
     },
     [KEY_FIRE_CHALLENGE_THRESHOLD]: {
       type: 'integer',
