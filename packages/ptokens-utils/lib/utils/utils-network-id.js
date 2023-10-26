884eee283069867e90625b9be6144856767328e8
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-10-26 16:07:26 +0200

    chore(ptokens-utils): add sync version for `getBlockchainTypeFromChainId`

--- packages/ptokens-utils/lib/utils/utils-network-id.js
+++ packages/ptokens-utils/lib/utils/utils-network-id.js
@@ -1,21 +1,32 @@
+const R = require('ramda')
 const { networkIds } = require('../constants')
 const constants = require('ptokens-constants')
 
+const getBlockchainTypeFromChainIdSync = _networkId => {
+  switch (_networkId) {
+    case networkIds.HARDHAT1:
+    case networkIds.HARDHAT2:
+    case networkIds.BSC_MAINNET:
+    case networkIds.GNOSIS_MAINNET:
+    case networkIds.POLYGON_MAINNET:
+    case networkIds.ARBITRUM_MAINNET:
+    case networkIds.ETHEREUM_MAINNET:
+    case networkIds.ETHEREUM_GOERLI:
+      return constants.blockchainType.EVM
+    default:
+      return null
+  }
+}
 const getBlockchainTypeFromChainId = _networkId =>
   new Promise((resolve, reject) => {
-    switch (_networkId) {
-      case networkIds.HARDHAT1:
-      case networkIds.HARDHAT2:
-      case networkIds.BSC_MAINNET:
-      case networkIds.GNOSIS_MAINNET:
-      case networkIds.POLYGON_MAINNET:
-      case networkIds.ARBITRUM_MAINNET:
-      case networkIds.ETHEREUM_MAINNET:
-      case networkIds.ETHEREUM_GOERLI:
-        return resolve(constants.blockchainType.EVM)
-      default:
-        return reject(new Error(`Unknown chain ID ${_networkId}`))
-    }
+    const blockchainType = getBlockchainTypeFromChainIdSync(_networkId)
+
+    return R.isNil(blockchainType)
+      ? reject(new Error(`Unknown chain ID ${_networkId}`))
+      : resolve(blockchainType)
   })
 
-module.exports = { getBlockchainTypeFromChainId }
+module.exports = {
+  getBlockchainTypeFromChainId,
+  getBlockchainTypeFromChainIdSync,
+}
