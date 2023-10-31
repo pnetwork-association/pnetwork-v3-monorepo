4ac55590899deb3deaf498819d20bf67c28d29b2
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-10-31 17:57:57 +0100

    refactor(ptokens-constants): move log parsing into `fromArgs`

--- packages/ptokens-constants/lib/hub/Challenge.js
+++ packages/ptokens-constants/lib/hub/Challenge.js
@@ -1,16 +1,24 @@
 class Challenge {
-  constructor({ args: [nonce, actor, challenger, actorType, timestamp, networkId] }) {
-    this.nonce = nonce
-    this.actor = actor
-    this.challenger = challenger
-    this.actorType = actorType
-    this.timestamp = timestamp
-    this.networkId = networkId
+  constructor(obj) {
+    obj && Object.assign(this, obj)
   }
 
-  getArg() {
+  static fromArgs({ args }) {
+    return new Challenge({
+      nonce: args[0].nonce,
+      actor: args[0].actor,
+      challenger: args[0].challenger,
+      actorType: args[0].actorType,
+      timestamp: args[0].timestamp,
+      networkId: args[0].networkId,
+    })
+  }
+
+  getArgs() {
     return [this.nonce, this.actor, this.challenger, this.actorType, this.timestamp, this.networkId]
   }
+
+  fromReceipt() {}
 }
 
 module.exports = Challenge
