0649fc38010cde182e830eadff710068ac583d87
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-11-13 19:09:06 +0100

    chore(ptokens-constants): add `actorStatus` struct

--- packages/ptokens-constants/lib/hub/index.js
+++ packages/ptokens-constants/lib/hub/index.js
@@ -3,10 +3,12 @@ const errors = require('./errors')
 const Challenge = require('./Challenge')
 const operationStatus = require('./operationStatus')
 const challengeStatus = require('./challenge-status')
+const actorsStatus = require('./actor-status')
 
 module.exports = {
   actors,
   errors,
+  actorsStatus,
   operationStatus,
   challengeStatus,
   Challenge,
