53e390a25c0e8ae8be703011e89d86b984826cca
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-11-14 14:52:48 +0100

    chore(ptokens-utils): export also `sub` in order to mock it later

--- packages/ptokens-utils/lib/ipfs/index.js
+++ packages/ptokens-utils/lib/ipfs/index.js
@@ -1,9 +1,11 @@
 const id = require('./id')
 const pubsub = require('./pubsub')
 const constants = require('./constants')
+const checkDaemon = require('./check-daemon')
 
 module.exports = {
   ...id,
+  ...checkDaemon,
   constants,
   pubsub,
 }
