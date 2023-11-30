1fc5b337fa5c6952ed52c9db524c0fd4e2fe03b5
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-11-30 11:53:33 +0100

    chore(ptokens-state-emitter): adapt to new IPFS RPC

--- packages/ptokens-state-emitter/lib/protocols/ipfs/index.js
+++ packages/ptokens-state-emitter/lib/protocols/ipfs/index.js
@@ -2,14 +2,23 @@ const R = require('ramda')
 const { logger } = require('../../get-logger')
 const { ipfs, utils, validation } = require('ptokens-utils')
 
+const KEY_DATA_URL = 'url'
+const KEY_DATA_TOPIC = 'topic'
+
 module.exports.publishStatus = R.curry((_protocolData, _status) =>
   validation
     .checkType('Object', _status)
-    .then(_ => utils.getKeyFromObj('topic', _protocolData))
-    .then(
-      _topic =>
-        logger.info(`Publishing status on IPFS(${_topic})...`) ||
-        ipfs.pubsub.pub(_topic, JSON.stringify(_status))
+    .then(_ =>
+      Promise.all([
+        utils.getKeyFromObj(KEY_DATA_URL, _protocolData),
+        utils.getKeyFromObj(KEY_DATA_TOPIC, _protocolData),
+      ])
     )
+    .then(([_url, _topic]) => {
+      logger.info(`Publishing status on IPFS(${_topic})...`)
+      const provider = new ipfs.IPFSProvider(_url)
+      const pubSub = new ipfs.PubSub(provider)
+      pubSub.pub(_topic, JSON.stringify(_status))
+    })
     .then(_ => logger.info('Data published successfully on IPFS!'))
 )
