6fbb3c6e6e4366cb17289a0914916aa10636497d
Mauro Piazza <mauro.piazza.01@gmail.com>
2023-11-30 11:15:29 +0100

    refactor(ptokens-utils): use IPFS RPC api instead of calling the binary directly

--- packages/ptokens-utils/lib/ipfs/index.js
+++ packages/ptokens-utils/lib/ipfs/index.js
@@ -1,11 +1,30 @@
-const id = require('./id')
-const pubsub = require('./pubsub')
-const constants = require('./constants')
-const checkDaemon = require('./check-daemon')
+const http = require('../http')
+const PubSub = require('./pubsub')
+const { ERROR_IPFS_DAEMON_DOWN } = require('../errors')
+const { FetchError } = require('node-fetch')
+const { rejectIfNil } = require('../utils')
+
+class IPFSProvider {
+  constructor(url) {
+    this.url = url
+  }
+
+  id() {
+    return http.fetchJsonByPost(`${this.url}/api/v0/id`, {})
+  }
+
+  checkDaemon() {
+    return this.id()
+      .then(rejectIfNil(ERROR_IPFS_DAEMON_DOWN))
+      .catch(_err =>
+        _err instanceof FetchError
+          ? Promise.reject(new Error(ERROR_IPFS_DAEMON_DOWN))
+          : Promise.reject(_err)
+      )
+  }
+}
 
 module.exports = {
-  ...id,
-  ...checkDaemon,
-  constants,
-  pubsub,
+  IPFSProvider,
+  PubSub,
 }
